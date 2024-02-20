bbff337549b8537491323d1bae7b7a201d899f4e
Opilane TTHK <opilane@tthk.ee>
2024-02-20 13:05:53 +0200

    started work on pythagorean theorem page

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿namespace OmniCalc;
+using OmniCalc.Views.PythagoreanTheorem;
 
 
 #if WINDOWS
@@ -19,17 +20,17 @@ public partial class App : Application
 
         Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
         {
-#if WINDOWS
-			var mauiWindow = handler.VirtualView;
-			var nativeWindow = handler.PlatformView;
-			nativeWindow.Activate();
-			IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
-			WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
-			AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
-			appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
-#endif
+            #if WINDOWS
+			            var mauiWindow = handler.VirtualView;
+			            var nativeWindow = handler.PlatformView;
+			            nativeWindow.Activate();
+			            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+			            WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+			            AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+			            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+            #endif
         });
 
-        MainPage = new NavigationPage(new StartPage());
+        MainPage = new NavigationPage(new PythagoreanTheoremPage());
     }
 }
