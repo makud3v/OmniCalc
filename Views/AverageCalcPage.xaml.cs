73145ce33d1ceceff089437d66ab1bf6e8bc286b
ingmarhendrik <ingmarrohusaar@gmail.com>
2024-02-26 12:30:22 +0200

    fd

--- Views/AverageCalcPage.xaml.cs
+++ Views/AverageCalcPage.xaml.cs
@@ -7,4 +7,21 @@ public partial class AverageCalcPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private string On_Calculated(object sender, EventArgs e)
+    {
+		var result = "";
+		foreach (var child in Content.GetVisualTreeDescendants())
+		{
+
+			string? id = (child as Entry)?.ClassId;
+
+			if (id != null || id != "AverageEntry") continue;
+			Entry c = child as Entry;
+			result += c.Text;
+			
+		}
+		return result;
+    }
 }
+
