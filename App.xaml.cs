1bfd8f2cc00d9a5bc7d264b78a386c6c6054e2a5
Christoferkrabbi <160104752+Christoferkrabbi@users.noreply.github.com>
2024-03-01 10:14:44 +0200

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -30,7 +30,7 @@ public partial class App : Application
             #endif
         });
 
-        MainPage = new NavigationPage(new AverageCalcPage()); 
+        MainPage = new NavigationPage(new StartPage()); 
         
     }
 }
